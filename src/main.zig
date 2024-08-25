6a05ed7c3106c5e72fa9038897c57d30b74df406
JJoeDev <JJoeDev@noreply.codeberg.org>
2024-08-25 22:10:06 +0200

    Better code for strings instead of bytes

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,26 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    var allocator = std.heap.page_allocator;
 
-    var file = try std.fs.cwd().openFile("data.txt", .{});
+    const buff = try readFile(&allocator, "data.txt");
+    defer allocator.free(buff);
+
+    var lines = std.mem.split(u8, buff, "\n");
+
+    while(lines.next()) |line| {
+        if(line.len <= 0) continue;
+
+        if(line[0] == '*') continue;
+        std.log.info("READ LINE: {s}", .{line});
+    }
+}
+
+fn readFile(allocator: *std.mem.Allocator, filename: []const u8) ![]u8 {
+    const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
-    var buffered = std.io.bufferedReader(file.reader());
-    var reader = buffered.reader();
-
-    var arr = std.ArrayList(u8).init(allocator);
-    defer arr.deinit();
-
-    var lineCount: usize = 0;
-    var byteCount: usize = 0;
-
-    while(true){
-        reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch(err){
-            error.EndOfStream => break,
-            else => return err,
-        };
-
-        lineCount += 1;
-        byteCount += arr.items.len;
-        arr.clearRetainingCapacity();
-    }
-
-    std.log.info("{d} lines | {d} bytes", .{lineCount, byteCount});
+    const stat = try file.stat();
+    const buff = try file.readToEndAlloc(allocator.*, stat.size);
+    return buff;
 }
