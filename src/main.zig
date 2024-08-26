6ab73e37c42e70ff30c14dd33ab2d7b7c8825885
JJoeDev <JJoeDev@noreply.codeberg.org>
2024-08-26 22:21:14 +0200

    Can now find key value pairs

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
+const Settings = struct {
+    key: []const u8,
+    value: []const u8,
+};
+
 pub fn main() !void {
-    var allocator = std.heap.page_allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var allocator = gpa.allocator();
 
     const buff = try readFile(&allocator, "data.txt");
     defer allocator.free(buff);
@@ -10,9 +18,21 @@ pub fn main() !void {
 
     while(lines.next()) |line| {
         if(line.len <= 0) continue;
-
         if(line[0] == '*') continue;
-        std.log.info("READ LINE: {s}", .{line});
+
+        const seperator = std.mem.indexOf(u8, line, ":");
+
+        if(seperator != null){
+            const key = line[0..seperator.?];
+            const value = line[seperator.? + 1..];
+
+            const kv = Settings{
+                .key = std.mem.trim(u8, key, " "),
+                .value = std.mem.trim(u8, value, " "),
+            };
+
+            std.log.info("Key: {s} | Value: {s}", .{kv.key, kv.value});
+        }
     }
 }
 
